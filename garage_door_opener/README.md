bf50f18067835c16754a53f8a1558b7cb08e853d
Deling Ren <12819582+delingren@users.noreply.github.com>
2024-03-25 18:33:21 -0700

    Update garage door opener

--- garage_door_opener/README.md
+++ garage_door_opener/README.md
@@ -2,24 +2,20 @@
 
 ## Objective
 
-My house has a dumb garage door opener. It works in a very simple way. The wall mount switch simply shorts two leads. When you press the button, if the motor is running (the door is being opened or closed), it stops the motor; otherwise, it starts the motor in the opposite direction. A remote control works the same way. This is probably how most dumb garage openers work.
-
-I wanted to integrate it with Apple's Home app so that I can operate it from my phone even if I'm not home and monitor its status as well.
+My house has a dumb garage door opener. And I wanted to integrate it with Apple's Home app so that I can monitor its status and operate it remotely. And I don't want to replace with a smart garage door opener or buy any off-the-shelf product.
 
 ## Mechanics and hardware
 
-My garage door opener is very rudimentary. The wall mount switch simply shorts two leads. When you press the button the following logic applies:
+My garage door opener is very rudimentary. The wall mount switch simply shorts two leads. When you press the button, the following logic applies:
 
-* If the door is closed, it runs the motor to open it.
-* If the door is open, it runs the motor to close it.
+* If the door is closed, it starts the motor to open it.
+* If the door is open, it starts the motor to close it.
 * If the motor is running (opening or closing), it stops the motor.
 * If the motor is stopped but the door is half open, it starts the motor opposite of the direction it was previously running.
 
+Also, when it's closing and it detects an obstruction, it reverses the direction of the motor and reverts to an open position.
+
 This is probably how most dumb garage openers work, or at least in a similar fasion. The remote works exactly as the button.
-
-In this project, I am using an ESP32 MCU to control the garage door opener via a relay. The status of the garage door is detected by two sensors, one placed at the open position and one placed at the closed position. They are reed switches. A magnet is mounted on the shuttle of the arm and the switches are mounted on the track.
-
-In addition, I am using two LEDs, one green and one red to physically indicate the position of the door. And I installed a push button to reset the MCU in case I need to reboot it for any reason. The switch simply shorts EN pin to GND. Other MCUs have different mechanisms for resetting externally.
 
 * Dev board  
 WiFi ESP WROOM-32
@@ -32,82 +28,52 @@ The brain of the project is ESP32 MCU. I am using this MCU for the following rea
 * There is a HAP library in Arduino framework.
 * It is cheap.
 
-To operate the door, I simply use a relay to simulate a button press. The relay is controled with a GPIO pin. The state of the garage door is detected by two reed switches, one placed at the open position and one placed at the closed position. A magnet is mounted on the shuttle of the opener and the switches are mounted on the track. 
+To operate the door, I simply use a relay to simulate a button press. The relay is controled with a GPIO pin. The state of the garage door is detected by two reed switches, one placed at the open position (upper) and one placed at the closed position (lower). A magnet is mounted on the shuttle of the opener and the switches are mounted on the track. 
 
 * Relay  
-I am using a [2 Channel DC 5V Relay Module](https://www.amazon.com/gp/product/B00E0NTPP4). But any 5V DC relay with one normally open channel will do.
+I am using this [5v Relay Module](https://www.amazon.com/dp/B00VRUAHLE). But any 5V DC relay with one normally open channel will do.
 
 * Sensors  
 I am using two [reed switches](https://www.amazon.com/gp/product/B0735BP1K4/). But any normally open switches will do.
 
-* LEDs and resistors  
-I am using two LEDs to indicate the status of the sensors. I also use them to signal errors. They are not necessary. But they make troubleshooting easier.
+* Misc
 
-Sensors:
-```
-   +- GPIO22
-S1 |
-   +- GND
-
-   +- GPIO23
-S2 |
-   +- GND
-```
-
-Relay:
-```
-    +- GPIO13
-    |
-RY1 +- +5v
-    |
-    +- GND 
-```
-
-LEDs:
-```
-     +- R1 - GPIO 18
-LED1 |
-     +- GND
-
-     +- R2 - GPIO 5
-LED2 |
-     +- GND
-
-```
+- Status LED
+- Push button
+- Reset button
 
 ## Sofwtare
 
 ### Development settings  
-* Arduino IDE, board: uPesy ESP32 Wroom DevKit.
-* WiFi Library
+* Arduino IDE
 * HomeSpan Library
 
 ### Design
 
 There are two relatively independent components:
 * State determination (detecting the current state of the door)
-* Operation (opening and closing the door)
+* Operations (opening and closing the door)
 
-Since the door can be manually closed and opened, we should not use the operations to change the state. Rather, the state should be purely determined by the sensors. Let's call the upper sensor U and the lower sensor L. U is high if the door is closed. L is high if the door is closed.
+Since the door can be manually closed and opened, we should not use the operations to change the state. Rather, the state should be purely determined by the sensors. Let's call the upper sensor U and the lower sensor L. U is low if the door is closed. L is low if the door is closed.
 
 We have the following 5 defined states, which are all recognized in [HAP](https://hexdocs.pm/hap/HAP.Characteristics.CurrentDoorState.html).
 
-* Open: U=1, L=0
-* Closed: U=0, L=1
-* Opening: U=0, L=0, previous state=closed
-* Closing: U=0, L=0, previous state=open
-* Stopped: U=0, L=0, previous state \in {opening, closing} and a certain period of time has passed since the last state transition (i.e. a timeout has happened)
+* OPEN: U=0, L=1
+* CLOSED: U=1, L=0
+* OPENING: U=1, L=1, previous state=CLOSED
+* CLOSING: U=1, L=1, previous state=OPEN
+* STOPPED: U=1, L=1, previous state \in {OPENING, CLOSING} and a certain period of time has passed since the last state transition (i.e. a timeout has happened)
 
-When the program starts, we check if the state is open or closed. If both U and L are 0, we assume state = stopped, until an event changes it to another state.
+When the program starts, we check if the state is OPEN or CLOSED. If both U and L are 1, we assume state = STOPPED, until an event changes it to another state.
 
 The way my garage door operates, while the door is being opened or closed, two things can happen.
 
 * A human presses the wall mount button or a remote and stops the door. 
-* The door hits an obstacle and reverses its direction.
+* The door hits an obstacle and reverses its direction while closing
 
-Therefore, we need to set a timeout after transitioning to opening or closing states. After the timeout, we should transition to stopped state. Every time we transition to opening or closing state, we start a timer. When the timer runs out, a timeout event is triggered.
+Therefore, we need to set a timeout after transitioning to OPENING or CLOSING states. After the timeout, we should transition to STOPPED state. Every time we transition to OPENING or CLOSING state, we start a timer. When the timer runs out, a timeout event is triggered. When we transition to OPEN, CLOSED, or STOPPED state, the timer stops.
 
-There have 4 events driven by the sensors and timeout. 
+There have 4 events driven by the sensors and timeout.
 
 * U 1->0 (UF)
 * U 0->1 (UR)
@@ -118,16 +84,16 @@ There have 4 events driven by the sensors and timeout.
 Instead of drawing a state machine, I'm just listing all the transitions in the following table.
 
 ```
-        |   UF   |   UR   |   LF   |   LR   |   TO   | 
+        |   UR   |   UF   |   LR   |   LF   |   TO   | 
 --------+--------+--------+--------+--------+--------+
-open    |closing |        |        |        |        |
-closed  |        |        |opening |        |        |
-opening |        |  open  |        | closed |stopped |
-closing |        |  open  |        | closed |stopped |
-stopped |        |  open  |        | closed |        |
+OPEN    |closing |        |        |        |        |
+CLOSED  |        |        |OPENING |        |        |
+OPENING |        |  OPEN  |        | CLOSED |STOPPED |
+CLOSING |        |  OPEN  |        | CLOSED |STOPPED |
+STOPPED |        |  OPEN  |        | CLOSED |        |
 --------+--------+--------+--------+--------+--------+
 ```
 
-All empty cells should be impossible transitions. Techincally, many are possible. E.g. stopped transitioning to opening or closing and opening to closing. But we have no way to tell, unless we install a device to detect the direction of the door's travel, which is complicated and unnecessary.
+All empty cells should be invalid transitions, as far as our unit is concerned. Some are due to lack of information. E.g. the door could transition from STOPPED to both OPENING or CLOSING. But since such operations don't go through our unit, we have no way to detect. So we stay in STOPPED until the opening or closing finishes and triggers a transition to OPEN or CLOSED.
 
 ### Test cases
---
+++ garage_door_opener/garage_door_opener.ino
@@ -0,0 +1,289 @@
+#include "src/HomeSpan.h"
+#include <Arduino.h>
+#include <vector>
+
+struct DEV_GarageDoor : Service::GarageDoorOpener {
+  // Debounce time for the contact sensors. Adjust based the particular sensor
+  // and probably the speed of the motor.
+  const unsigned long millis_debounce = 600;
+  // The longest time between triggering a sensor, after an opening or closing
+  // event starts. For my garage door, it reverses the course when it hits an
+  // obstruction, so this should be a little longer than the time it takes to
+  // (almost) fully close and open again.
+  const unsigned long millis_timeout = 5000;
+
+  // These values are the same as the enum defined in
+  // GarageDoorOpener::CurrentDoorState. We would use that enum but it's
+  // anonymous.
+  enum door_state {
+    OPEN = 0,
+    CLOSED = 1,
+    OPENING = 2,
+    CLOSING = 3,
+    STOPPED = 4
+  };
+
+  static std::vector<DEV_GarageDoor *> all_openers;
+
+  static void isr() {
+    // The interrupt handler for all sensor pins. I don't think there is an easy
+    // way to create an ISR for each individual pin. So they'll have to share an
+    // ISR.
+    for (auto it = all_openers.begin(); it != all_openers.end(); it++) {
+      uint8_t new_value_upper = digitalRead((*it)->pin_upper);
+      uint8_t new_value_lower = digitalRead((*it)->pin_lower);
+
+      // loop() will do the debouncing logic. We are only informing it that one
+      // of the sensors has changed state and when.
+      if (new_value_lower != (*it)->value_lower ||
+          new_value_upper != (*it)->value_upper) {
+        (*it)->sensor_triggered = true;
+        (*it)->millis_last = millis();
+      }
+    }
+  }
+
+  Characteristic::CurrentDoorState current_state;
+  Characteristic::TargetDoorState target_state;
+  Characteristic::ObstructionDetected obstruction;
+
+  uint8_t pin_upper;
+  uint8_t pin_lower;
+  uint8_t pin_relay;
+
+  uint8_t value_upper;
+  uint8_t value_lower;
+
+  door_state state = door_state::STOPPED;
+
+  bool sensor_triggered = false;
+  unsigned long millis_last = 0;
+  unsigned long millis_timer = 0;
+  bool pending_operation = false;
+
+  DEV_GarageDoor(uint8_t pin_upper, uint8_t pin_lower, uint8_t pin_relay)
+      : Service::GarageDoorOpener() {
+    this->pin_upper = pin_upper;
+    this->pin_lower = pin_lower;
+    this->pin_relay = pin_relay;
+
+    pinMode(pin_upper, INPUT_PULLUP);
+    pinMode(pin_lower, INPUT_PULLUP);
+
+    value_upper = digitalRead(pin_upper);
+    value_lower = digitalRead(pin_lower);
+
+    pinMode(pin_relay, OUTPUT);
+    digitalWrite(pin_relay, LOW);
+
+    // Determine the initial state.
+    if (value_upper == LOW && value_lower == HIGH) {
+      state = door_state::OPEN;
+    } else if (value_upper == HIGH && value_lower == LOW) {
+      state = door_state::CLOSED;
+    } else {
+      // The door could be OPENING, CLOSING, or STOPPED. But there is no way we
+      // can tell. So we assume it's STOPPED. If it's indeed OPENING or CLOSING,
+      // it'll trigger a sensor change event (if successful) and we will be able
+      // to transition into the OPEN or CLOSED state. If unsuccessful, STOPPED
+      // will be the correct state.
+      state = door_state::STOPPED;
+    }
+    current_state.setVal(state);
+
+    all_openers.push_back(this);
+    attachInterrupt(pin_upper, isr, CHANGE);
+    attachInterrupt(pin_lower, isr, CHANGE);
+  }
+
+  void trigger_relay() {
+    Serial.println("Turning on the relay for 400 ms.");
+    digitalWrite(pin_relay, HIGH);
+    delay(400);
+    digitalWrite(pin_relay, LOW);
+  }
+
+  static unsigned long millis_since(unsigned long millis_then) {
+    unsigned long millis_now = millis();
+    // millis() overflows after about 50 days. So, if millis_now < millis_last,
+    // that's what happened.
+    return millis_now >= millis_then
+               ? millis_now - millis_then
+               : 0xFFFFFFFF - (millis_then - millis_now) + 1;
+  }
+
+  void reach_target_state(uint8_t target_state) {
+    if (!pending_operation) {
+      return;
+    }
+
+    // 1. If the target state matches the current state, do nothing.
+    // 2. If the target state is the opposite of the current state, trigger the
+    // relay to simulate a button press.
+    // 3. If the current state is either OPENING or CLOSING, the safest approach
+    // is to do nothing for now, but wait till it's reached one of the three
+    // other states and come back here.
+    // 4. If we are in the STOPPED state, trigger the relay. But we don't know
+    // which direction the door will be moving. So we do that anyway and wait
+    // till it's reached OPEN or CLOSED state. If the state doesn't match the
+    // target state, we trigger the relay again.
+
+    // TODO: if there is an obstruction, and we try to close, we could be stuck
+    // in an infinite loop: OPEN -> CLOSING -> (reversing, but unknown to us) ->
+    // OPEN. Should we not try at all or do it anway and devise a mechanism to
+    // detect such infinite loops?
+    if (target_state == Characteristic::TargetDoorState::OPEN) {
+      if (state == door_state::OPEN) {
+        Serial.println("-- Open operation completed.");
+        pending_operation = false;
+      }
+      if (state == door_state::CLOSED || state == door_state::STOPPED) {
+        Serial.printf(
+            "Trying to reach target state %d from current state %d.\n",
+            target_state, state);
+        trigger_relay();
+      }
+    } else {
+      if (state == door_state::CLOSED) {
+        Serial.println("-- Close operation completed.");
+        pending_operation = false;
+      }
+      if (state == door_state::OPEN || state == door_state::STOPPED) {
+        Serial.printf(
+            "Trying to reach target state %d from current state %d.\n",
+            target_state, state);
+        trigger_relay();
+      }
+    }
+  }
+
+  void transition(door_state new_state) {
+    Serial.printf("-- Transitioning from %d to %d.\n", state, new_state);
+
+    // When closing, my opener reverses the course if it detects an
+    // obstruction. So, if we see a CLOSING -> OPEN transition, we consider
+    // that an obstruction has been detected. However, this scenario is also
+    // possible with manual intervention with the wall mount switch or a
+    // remote. But there is no way for us to distinguish. In that case, we
+    // will let the human correct the situation by pressing the switch again.
+    if (state == door_state::CLOSING && new_state == door_state::OPEN) {
+      Serial.println("-- Obstruction detected.");
+    }
+
+    obstruction.setVal(state == door_state::CLOSING &&
+                       new_state == door_state::OPEN);
+
+    if (new_state == door_state::OPENING || new_state == door_state::CLOSING) {
+      Serial.println("-- Starting the timer.");
+      millis_timer = millis();
+    } else {
+      Serial.println("-- Stopping the timer.");
+      millis_timer = 0;
+    }
+
+    state = new_state;
+    current_state.setVal(state);
+
+    // Check if we have a pending operation.
+    reach_target_state(target_state.getVal());
+  }
+
+  boolean update() {
+    pending_operation = true;
+    reach_target_state(target_state.getNewVal());
+    return true;
+  }
+
+  void loop() {
+    // State transitions.
+    //         |   UR   |   UF   |   LR   |   LF   |   TO   |
+    // --------+--------+--------+--------+--------+--------+
+    // open    |closing |        |        |        |        |
+    // closed  |        |        |opening |        |        |
+    // opening |        |  open  |        | closed |stopped |
+    // closing |        |  open  |        | closed |stopped |
+    // stopped |        |  open  |        | closed |        |
+    // --------+--------+--------+--------+--------+--------+
+
+    // All empty cells are invalid. However, we always transition the the state
+    // determined by the previous pin values and current ones, even if we are in
+    // an invalid state. It's the only way we can recover. But we report such
+    // errors.
+
+    // Timeout event: when transitioning into OPENING or CLOSING, we start a
+    // timer. The timer is stopped if we reach OPEN or CLOSED state. But once it
+    // runs out, we consider the door in the STOPPED state. This could be caused
+    // by manual intervention with a remote or wall switch, a power outage, a
+    // motor failure, or a number of other reasons.
+    if (!sensor_triggered && millis_timer != 0 &&
+        millis_since(millis_timer) > millis_timeout) {
+      Serial.println("-- Timer ran out.");
+      if (state != door_state::CLOSING && state != door_state::OPENING) {
+        Serial.printf("-- Timeout. Invalid state: %d.\n", state);
+      }
+      transition(door_state::STOPPED);
+    }
+
+    // Sensor change event. This happens when the door reaches or leaves either
+    // end of the track. It's not practically possible to have two transitions
+    // in one iteration, since it takes at least a few seconds to open or close
+    // a garage door. hence we only consider one transition here.
+    if (sensor_triggered && millis_since(millis_last) > millis_debounce) {
+      // Debouncing logic: it's been enough time since the last change, so it
+      // should be stable now.
+      uint8_t new_value_upper = digitalRead(pin_upper);
+      uint8_t new_value_lower = digitalRead(pin_lower);
+
+      if (value_upper == LOW && new_value_upper == HIGH) {
+        // Upper, Rising
+        if (state != door_state::OPEN) {
+          Serial.printf("-- Upper sensor falling. Invalid state: %d.\n", state);
+        }
+        transition(door_state::CLOSING);
+      } else if (value_upper == HIGH && new_value_upper == LOW) {
+        // Upper, Falling
+        if (state != door_state::OPENING && state != door_state::CLOSING &&
+            state != door_state::STOPPED) {
+          Serial.printf("-- Upper sensor rising. Invalid state: %d.\n", state);
+        }
+        transition(door_state::OPEN);
+      } else if (value_lower == LOW && new_value_lower == HIGH) {
+        // Lower, Rising
+        if (state != door_state::CLOSED) {
+          Serial.printf("-- Lower sensor falling. Invalid state: %d.\n", state);
+        }
+        transition(door_state::OPENING);
+      } else if (value_lower == HIGH && new_value_lower == LOW) {
+        // Lower, Falling
+        if (state != door_state::OPENING && state != door_state::CLOSING &&
+            state != door_state::STOPPED) {
+          Serial.printf("-- Lower sensor rising. Invalid state: %d. \n", state);
+        }
+        transition(door_state::CLOSED);
+      }
+
+      value_upper = new_value_upper;
+      value_lower = new_value_lower;
+      sensor_triggered = false;
+    }
+  }
+};
+
+std::vector<DEV_GarageDoor *> DEV_GarageDoor::all_openers;
+
+void setup() {
+  Serial.begin(115200);
+
+  homeSpan.setStatusPin(25).setControlPin(26).begin(Category::GarageDoorOpeners,
+                                                    "Garage Door");
+
+  new SpanAccessory();
+  new Service::AccessoryInformation();
+  new Characteristic::Identify();
+  new Characteristic::Name("Garage Door");
+  new DEV_GarageDoor(22, 23, 13);
+
+  homeSpan.autoPoll();
+}
+
+void loop() {}
